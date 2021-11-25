eb151d871bdc94e279fd1c9d8c0aea58e4d033b5
Emma Li <emma.m.li.98@gmail.com>
2021-11-24 20:47:24 -0500

    Setup home page

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const port = 3000
 
 app.use('/a', express.static('pages'))
 app.use('/static', express.static('public'))
+app.use(express.static('pages/home'))
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
